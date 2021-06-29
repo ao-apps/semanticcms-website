dafb098d25da9bade68093e67ab9d685e874cdc9
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:42:00 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,99 @@
+/*
+ * semanticcms-website - The semanticcms.com website.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-website.
+ *
+ * semanticcms-website is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-website is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-website.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.semanticcms.website {
+	requires com.aoapps.taglib; // <groupId>com.aoapps</groupId><artifactId>ao-taglib</artifactId>
+	requires com.semanticcms.all.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-all-book</artifactId>
+	requires com.semanticcms.autogit.all.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-autogit-all-book</artifactId>
+	requires com.semanticcms.autogit.bom.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-autogit-bom-book</artifactId>
+	requires com.semanticcms.autogit.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-autogit-book</artifactId>
+	requires com.semanticcms.autogit.model.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-autogit-model-book</artifactId>
+	requires com.semanticcms.autogit.servlet.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-autogit-servlet-book</artifactId>
+	requires com.semanticcms.autogit.style.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-autogit-style-book</artifactId>
+	requires com.semanticcms.autogit.taglib.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-autogit-taglib-book</artifactId>
+	requires com.semanticcms.autogit.view.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-autogit-view-book</artifactId>
+	requires com.semanticcms.bom.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-bom-book</artifactId>
+	requires com.semanticcms.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-book</artifactId>
+	requires com.semanticcms.changelog.taglib.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-changelog-taglib-book</artifactId>
+	requires com.semanticcms.core.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-all</artifactId>
+	requires com.semanticcms.core.all.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-all-book</artifactId>
+	requires com.semanticcms.core.bom.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-bom-book</artifactId>
+	requires com.semanticcms.core.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-book</artifactId>
+	requires com.semanticcms.core.breadcrumblist.jsonld.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-breadcrumblist-json-ld-book</artifactId>
+	requires com.semanticcms.core.canonical.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-canonical-book</artifactId>
+	requires com.semanticcms.core.docs.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-docs-book</artifactId>
+	requires com.semanticcms.core.model.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-model-book</artifactId>
+	requires com.semanticcms.core.servlet.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-servlet-book</artifactId>
+	requires com.semanticcms.core.sitemap.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-sitemap-book</artifactId>
+	requires com.semanticcms.core.style.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-style-book</artifactId>
+	requires com.semanticcms.core.taglib.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-taglib-book</artifactId>
+	requires com.semanticcms.core.theme.base.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-theme-base-book</artifactId>
+	requires com.semanticcms.core.view.content.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-view-content-book</artifactId>
+	requires com.semanticcms.dia.all.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-all-book</artifactId>
+	requires com.semanticcms.dia.bom.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-bom-book</artifactId>
+	requires com.semanticcms.dia.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-book</artifactId>
+	requires com.semanticcms.dia.model.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-model-book</artifactId>
+	requires com.semanticcms.dia.servlet.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-servlet-book</artifactId>
+	requires com.semanticcms.dia.style.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-style-book</artifactId>
+	requires com.semanticcms.dia.taglib.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-taglib-book</artifactId>
+	requires com.semanticcms.dia.view.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-dia-view-book</artifactId>
+	requires com.semanticcms.file.all.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-all-book</artifactId>
+	requires com.semanticcms.file.bom.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-bom-book</artifactId>
+	requires com.semanticcms.file.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-book</artifactId>
+	requires com.semanticcms.file.model.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-model-book</artifactId>
+	requires com.semanticcms.file.servlet.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-servlet-book</artifactId>
+	requires com.semanticcms.file.style.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-style-book</artifactId>
+	requires com.semanticcms.file.taglib.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-taglib-book</artifactId>
+	requires com.semanticcms.file.view.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-view-book</artifactId>
+	requires com.semanticcms.googleanalytics.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-google-analytics-book</artifactId>
+	requires com.semanticcms.news.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-all</artifactId>
+	requires com.semanticcms.news.all.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-all-book</artifactId>
+	requires com.semanticcms.news.bom.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-bom-book</artifactId>
+	requires com.semanticcms.news.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-book</artifactId>
+	requires com.semanticcms.news.model.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-model-book</artifactId>
+	requires com.semanticcms.news.rss.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-rss-book</artifactId>
+	requires com.semanticcms.news.servlet.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-servlet-book</artifactId>
+	requires com.semanticcms.news.style.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-style-book</artifactId>
+	requires com.semanticcms.news.taglib.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-taglib-book</artifactId>
+	requires com.semanticcms.news.view.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-view-book</artifactId>
+	requires com.semanticcms.openfile.all.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-openfile-all-book</artifactId>
+	requires com.semanticcms.openfile.bom.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-openfile-bom-book</artifactId>
+	requires com.semanticcms.openfile.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-openfile-book</artifactId>
+	requires com.semanticcms.openfile.servlet.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-openfile-servlet-book</artifactId>
+	requires com.semanticcms.openfile.taglib.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-openfile-taglib-book</artifactId>
+	requires com.semanticcms.pagegraph.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-pagegraph-book</artifactId>
+	requires com.semanticcms.parent.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-parent-book</artifactId>
+	requires com.semanticcms.section.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-all</artifactId>
+	requires com.semanticcms.section.all.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-all-book</artifactId>
+	requires com.semanticcms.section.bom.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-bom-book</artifactId>
+	requires com.semanticcms.section.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-book</artifactId>
+	requires com.semanticcms.section.model.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-model-book</artifactId>
+	requires com.semanticcms.section.servlet.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-servlet-book</artifactId>
+	requires com.semanticcms.section.style.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-style-book</artifactId>
+	requires com.semanticcms.section.taglib.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-taglib-book</artifactId>
+	requires com.semanticcms.tagreference.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-tag-reference-book</artifactId>
+	requires com.semanticcms.theme.documentation.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-theme-documentation-book</artifactId>
+	requires com.semanticcms.theme.documentation.style.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-theme-documentation-style-book</artifactId>
+	requires com.semanticcms.view.all.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-view-all-book</artifactId>
+	requires com.semanticcms.view.tree.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-view-tree-book</artifactId>
+	requires com.semanticcms.view.what_links_here.book; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-view-what-links-here-book</artifactId>
+}
